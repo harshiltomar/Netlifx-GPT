355752ae9db9838a23d0e3117e4c858b8de55d0d
Harshil Tomar <harshiltomar20@gmail.com>
2023-12-08 16:56:49 +0530

    4.2

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -4,28 +4,30 @@ import { addTrailerVideo } from "../utils/moviesSlice";
 import { useEffect } from "react";
 
 const useMovieTrailer = (movieId) => {
-  //Fetch Trailer Video and updating the storw with Trailer Video Data
-
   const dispatch = useDispatch();
 
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/" +
-        movieId +
-        "/videos?language=en-US",
-      API_OPTIONS
-    );
-
-    const json = await data.json();
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    dispatch(addTrailerVideo(trailer));
-  };
-
   useEffect(() => {
+    const getMovieVideos = async () => {
+      try {
+        const data = await fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+          API_OPTIONS
+        );
+
+        const json = await data.json();
+
+        const filterData = json.results.filter(
+          (video) => video.type === "Trailer"
+        );
+        const trailer = filterData.length ? filterData[0] : json.results[0];
+        dispatch(addTrailerVideo(trailer));
+      } catch (error) {
+        console.error("Error fetching movie videos:", error);
+      }
+    };
+
     getMovieVideos();
-  }, [getMovieVideos]);
+  }, [dispatch, movieId]);
 };
 
 export default useMovieTrailer;
